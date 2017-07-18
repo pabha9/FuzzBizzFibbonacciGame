3939caf1aa66f4aee2eb65dcb642f3f724856a83
pabha9 <pabha9@yahoo.com>
2017-07-18 14:28:09 -0600

    Update swift_nav.py

--- swift_nav.py
+++ swift_nav.py
@@ -28,12 +28,12 @@ def divisibleChecker(n):
     if a == False:
        if n % 15 ==0: 
             print("Buzz, Fizz, FizzBuzz")
-       elif (not(n % 3 ==0) and not(n%5 ==0) and not(n % 15 ==0)): #Check for all three because it was giving me bugs and printing corresponding fibbonacci numbers regardless
-            print("corresponding fibbonacci number:", n)
        elif n % 3 == 0: 
                 print("Buzz")
        elif n % 5 == 0:
                  print("Fizz")
+       else: 
+                print("corresponding fibbonacci number:", n)
     elif a == True: 
        
         if n == 3: 
